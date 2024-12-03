8ac1306059ee9d7eacbde0398a4712f8805fad12
Sven van Ginkel <svenvanginkel@icloud.com>
2024-12-03 08:56:24 +0100

    push

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schedule;
 
 /**
  * Checked for new versions weekly on Thursday because
- * I usually do releases on Thursday or Friday.
+ * I usually do releases on Thursday or Friday.Mesh
  */
 Schedule::command('app:version')
     ->weeklyOn(5);
